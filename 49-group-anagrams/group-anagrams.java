f4ee0e2c73dd0512cc3374209497717f274f88f0
Ravi Akella <raviakella03@users.noreply.github.com>
2024-02-06 16:09:50 +0530

    Time: 6 ms (92.74%) | Memory: 47.5 MB (54.86%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,7 +1,6 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
         Map<String, List<String>> hashMap = new HashMap<>();
-        List<List<String>> anagrams = new ArrayList<>();
         int inputLength = strs.length;
         for(int i = 0; i < inputLength; i++) {
             char[] stringArray = strs[i].toCharArray();
@@ -11,10 +10,6 @@ class Solution {
             value.add(strs[i]);
             hashMap.put(key, value);
         }
-
-        for(String key: hashMap.keySet()) {
-            anagrams.add(hashMap.get(key));
-        }
-        return anagrams;
+        return new ArrayList<List<String>>(hashMap.values());
     }
 }
