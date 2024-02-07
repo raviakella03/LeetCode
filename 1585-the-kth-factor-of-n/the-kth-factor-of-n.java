7ee9b9a7e3536ff94ce3f188b1596e52b8e0f055
Ravi Akella <raviakella03@users.noreply.github.com>
2024-02-07 18:56:24 +0530

    Time: 0 ms (100.00%) | Memory: 40.2 MB (73.15%) - LeetSync

--- 1585-the-kth-factor-of-n/the-kth-factor-of-n.java
+++ 1585-the-kth-factor-of-n/the-kth-factor-of-n.java
@@ -1,15 +1,15 @@
 class Solution {
     public int kthFactor(int n, int k) {
-        // int count = 0;
-        // int kthFactor = 0;
-        // for (int j = 1; count <= k + 1 && j <= n; j++) {
-        // if (n % j == 0) {
-        // if (++count == k && kthFactor == 0) {
-        // kthFactor = j;
-        // }
-        // }
-        // }
-        // return count >= k ? kthFactor : -1;
+        int count = 0;
+        int kthFactor = 0;
+        for (int j = 1; count <= k + 1 && j <= n; j++) {
+            if (n % j == 0) {
+                if (++count == k && kthFactor == 0) {
+                    kthFactor = j;
+                }
+            }
+        }
+        return count >= k ? kthFactor : -1;
 
         // int count = 0;
         // for (int i = 1; count <= k + 1 && i <= n; i++) {
@@ -21,11 +21,12 @@ class Solution {
         // }
         // }
         // return -1;
-        for (int i = 1; i <= n / 2; i++) {
-            if (n % i == 0 && --k == 0) {
-                return i;
-            }
-        }
-        return k == 1 ? n : -1;
+
+        // for (int i = 1; i <= n / 2; i++) {
+        // if (n % i == 0 && --k == 0) {
+        // return i;
+        // }
+        // }
+        // return k == 1 ? n : -1;
     }
 }
