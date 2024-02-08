fa4e34208f7d76aa11ab6087a322264aa02d54c9
Ravi Akella <raviakella03@users.noreply.github.com>
2024-02-08 22:51:04 +0530

    Time: 37 ms (55.63%) | Memory: 42.6 MB (64.19%) - LeetSync

---
+++ 279-perfect-squares/perfect-squares.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int numSquares(int n) {
+        int[] dpArr = new int[n + 1];
+        dpArr[0] = 0;
+        for(int i = 1; i <= n;) {
+            dpArr[i++] = n;
+        }
+
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= i; j++) {
+                int numSqr = j * j;
+                if(i - numSqr < 0) {
+                    break;
+                }
+                dpArr[i] = Math.min(dpArr[i], dpArr[i - numSqr] + 1);
+            }
+        }
+        return dpArr[n];
+    }
+}
