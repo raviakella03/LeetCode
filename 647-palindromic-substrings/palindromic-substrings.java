2f76863a33a7658122ff76c39e030cf28e663090
Ravi Akella <raviakella03@users.noreply.github.com>
2024-02-10 13:38:45 +0530

    Time: 7 ms (44.06%) | Memory: 40.6 MB (97.59%) - LeetSync

---
+++ 647-palindromic-substrings/palindromic-substrings.java
@@ -0,0 +1,60 @@
+class Solution {
+    public int countSubstrings(String s) {
+        int strLen = s.length();
+        return findOddLengthPalindromes(s) + findEvenLengthPalindromes(s);
+    }
+
+    private int findOddLengthPalindromes(String s) {
+        int inputStringLength = s.length();
+        int oddLengthPalindromesCount = 0;
+        int maxOddLengthStartIndex = 0;
+        int maxOddLengthEndIndex = 0;
+        for (int centreIndex = 0; centreIndex < inputStringLength; centreIndex++) {
+            int leftPointer = centreIndex;
+            int rightPointer = centreIndex;
+            while (leftPointer >= 0 && rightPointer < inputStringLength) {
+                if (s.charAt(leftPointer) == s.charAt(rightPointer)) {
+                    // if (maxOddLengthOfPalindromicSubString < rightPointer - leftPointer + 1) {
+                    // maxOddLengthOfPalindromicSubString = rightPointer - leftPointer + 1;
+                    // maxOddLengthStartIndex = leftPointer;
+                    // maxOddLengthEndIndex = rightPointer;
+                    // }
+                    ++oddLengthPalindromesCount;
+                    --leftPointer;
+                    ++rightPointer;
+                } else {
+                    break;
+                }
+            }
+        }
+        return oddLengthPalindromesCount;
+    }
+
+    private int findEvenLengthPalindromes(String s) {
+        int inputStringLength = s.length();
+        int evenLengthPalindromesCount = 0;
+        int maxEvenLengthStartIndex = 0;
+        int maxEvenLengthEndIndex = 0;
+        for (int leftCentre = 0, rightCentre = 1; rightCentre < inputStringLength; leftCentre++, rightCentre++) {
+            if (s.charAt(leftCentre) == s.charAt(rightCentre)) {
+                int leftPointer = leftCentre;
+                int rightPointer = rightCentre;
+                while (leftPointer >= 0 && rightPointer < inputStringLength) {
+                    if (s.charAt(leftPointer) == s.charAt(rightPointer)) {
+                        // if (maxEvenLengthOfPalindromicSubString < rightPointer - leftPointer + 1) {
+                        // maxEvenLengthOfPalindromicSubString = rightPointer - leftPointer + 1;
+                        // maxEvenLengthStartIndex = leftPointer;
+                        // maxEvenLengthEndIndex = rightPointer;
+                        // }
+                        ++evenLengthPalindromesCount;
+                        --leftPointer;
+                        ++rightPointer;
+                    } else {
+                        break;
+                    }
+                }
+            }
+        }
+        return evenLengthPalindromesCount;
+    }
+}
